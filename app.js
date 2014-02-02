fd7f50cf0f88f92c7053d021fdafd8fb2dac3132
nikitakuz <skateaddict@gmail.com>
2014-02-01 22:40:07 -0800

    Make requests to www.jadeless.com and report status code.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var path = require('path');
+var http = require('http');
 var express = require('express');
 
 var app = express();
@@ -9,9 +9,29 @@ app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.session({ secret: 't0ps3kr1t' }));
 
+var hosts = [
+  'www.jadeless.com'
+];
+
 app.get('/', function(req, res) {
+  for (var i = 0; i < hosts.length; i++) {
+    pingHost(hosts[i]);
+  }
   res.send(200, 'ping!');
 });
+
+function pingHost(host) {
+  var req = http.request({hostname: host}, function(res) {
+    console.log('Status code ' + res.statusCode + ' received from ' + host);
+  });
+
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
+
+  console.log('Sending request to ' + host);
+  req.end();
+}
 
 var port = process.env.PORT || 5000;
 
